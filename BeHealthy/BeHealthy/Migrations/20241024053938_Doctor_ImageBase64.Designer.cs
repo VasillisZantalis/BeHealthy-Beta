cb1ec0e896bc8fd5e6635ed4c938f7aaa7719345
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-10-24 08:50:04 +0300

    added image to the doctor table

--- BeHealthy/BeHealthy/Migrations/20241024053938_Doctor_ImageBase64.Designer.cs
+++ BeHealthy/BeHealthy/Migrations/20241024053938_Doctor_ImageBase64.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BeHealthy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeHealthy.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241024053938_Doctor_ImageBase64")]
+    partial class Doctor_ImageBase64
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
