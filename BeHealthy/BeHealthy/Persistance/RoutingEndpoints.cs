911392b7e57864f6780b35d6ea4a04e513298dea
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-11-08 16:53:27 +0200

    Added BackButton

--- BeHealthy/BeHealthy/Persistance/RoutingEndpoints.cs
+++ BeHealthy/BeHealthy/Persistance/RoutingEndpoints.cs
@@ -4,6 +4,7 @@ public static class RoutingEndpoints
 {
     public const string HOME_PAGE = "/";
     public const string APPOINTMENTS_PAGE = "/appointments";
+    public const string DEPARTMENTS_PAGE = "/departments";
     public const string DOCTORS_PAGE = "/doctors";
     public const string PATIENTS_PAGE = "/patients";
     public const string NURSES_PAGE = "/nurses";
