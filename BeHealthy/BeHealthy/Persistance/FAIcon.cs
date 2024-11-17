d128dabf69445476397ff3e4757a6355c8363bcf
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-11-17 16:28:52 +0200

    Icons component for dry and static typed Icons added

---
+++ BeHealthy/BeHealthy/Persistance/FAIcon.cs
@@ -0,0 +1,28 @@
+﻿namespace BeHealthy.Persistance;
+
+public static class FAIcon
+{
+    public static string Ban => "fa-solid fa-ban";
+    public static string Globe => "fa-solid fa-globe";
+    public static string RightFromBracket => "fa-solid fa-right-from-bracket";
+    public static string Hospital => "fa-regular fa-hospital";
+    public static string BedPulse => "fa-solid fa-bed-pulse";
+    public static string CalendarDays => "fa-solid fa-calendar-days";
+    public static string Gear => "fa-solid fa-gear";
+    public static string House => "fa-solid fa-house";
+    public static string UserDoctor => "fa-solid fa-user-doctor";
+    public static string UserLock => "fa-solid fa-user-lock";
+    public static string UserNurse => "fa-solid fa-user-nurse";
+    public static string Viruses => "fa-solid fa-viruses";
+    public static string Pencil => "fa-solid fa-pencil";
+    public static string Plus => "fa-solid fa-plus";
+    public static string TrashCan => "fa-solid fa-trash-can";
+    public static string CircleInfo => "fa-solid fa-circle-info";
+    public static string EllipsisVertical => "fa-solid fa-ellipsis-vertical";
+    public static string CircleCheck => "fa-regular fa-circle-check";
+    public static string CircleXmark => "fa-regular fa-circle-xmark";
+    public static string TriangleExclamation => "fa-solid fa-triangle-exclamation";
+    public static string ChevronLeft => "fa-solid fa-chevron-left";
+    public static string Envelope => "fa-regular fa-envelope";
+    public static string MobileScreenButton => "fa-solid fa-mobile-screen-button";
+}
