a075a3645555fee623d310d28fd7c82bc6e55f7a
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-12-04 19:29:12 +0200

    Refactoring for adding static roles and no magical strings

---
+++ BeHealthy/BeHealthy/Extensions/UserRoleExtensions.cs
@@ -0,0 +1,11 @@
+﻿using BeHealthy.Domain;
+
+namespace BeHealthy.Extensions;
+
+public static class UserRoleExtensions
+{
+    public static string ToRoleString(this UserRole[] roles)
+    {
+        return string.Join(",", roles.Select(r => r.ToString()));
+    }
+}
