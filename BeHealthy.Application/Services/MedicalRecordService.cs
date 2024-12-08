b4e2ad59b482ca8c40d98db5970ed0b509773422
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-12-08 12:56:18 +0200

    MedicalRecordService using the UnitOfWork

--- BeHealthy.Application/Services/MedicalRecordService.cs
+++ BeHealthy.Application/Services/MedicalRecordService.cs
@@ -3,44 +3,46 @@ using BeHealthy.Application.Services.Interfaces;
 using BeHealthy.Domain.Interfaces.Repositories;
 using BeHealthy.Domain.Entities;
 using BeHealthy.Application.Mappings;
+using BeHealthy.Domain.Interfaces;
+using BeHealthy.Infrastructure.Repositories;
 
 namespace BeHealthy.Application.Services;
 
 public class MedicalRecordService : IMedicalRecordService
 {
-    private readonly IMedicalRecordRepository _medicalRecordRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public MedicalRecordService(IMedicalRecordRepository medicalRecordRepository)
+    public MedicalRecordService(IUnitOfWork unitOfWork)
     {
-        _medicalRecordRepository = medicalRecordRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<IEnumerable<MedicalRecordDto>> GetAllMedicalRecordsAsync()
     {
-        var medicalRecords = await _medicalRecordRepository.GetAllAsync();
+        var medicalRecords = await _unitOfWork.MedicalRecordRepository.GetAllAsync();
         return medicalRecords.MapToDto();
     }
 
     public async Task<MedicalRecordDto> GetMedicalRecordByIdAsync(int id)
     {
-        var medicalRecord = await _medicalRecordRepository.GetByIdAsync(id);
+        var medicalRecord = await _unitOfWork.MedicalRecordRepository.GetByIdAsync(id);
         return medicalRecord.MapToDto();
     }
 
     public async Task AddMedicalRecordAsync(MedicalRecordForCreationDto medicalRecordDto)
     {
         var medicalRecord = medicalRecordDto.MapToDomain();
-        await _medicalRecordRepository.AddAsync(medicalRecord);
+        await _unitOfWork.MedicalRecordRepository.AddAsync(medicalRecord);
     }
 
     public async Task UpdateMedicalRecordAsync(MedicalRecordForUpdateDto medicalRecordDto)
     {
         var medicalRecord = medicalRecordDto.MapToDomain();
-        await _medicalRecordRepository.UpdateAsync(medicalRecord);
+        await _unitOfWork.MedicalRecordRepository.UpdateAsync(medicalRecord);
     }
 
     public async Task DeleteMedicalRecordAsync(int id)
     {
-        await _medicalRecordRepository.DeleteAsync(id);
+        await _unitOfWork.MedicalRecordRepository.DeleteAsync(id);
     }
 }
