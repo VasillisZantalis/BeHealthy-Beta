12b4953ded3aef8a8e2f0a85434eb0c0cdde2c99
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-12-12 16:19:16 +0200

    full calendar

---
+++ BeHealthy/BeHealthy/wwwroot/js/calendar.js
@@ -0,0 +1,13 @@
+﻿function initializeFullCalendar() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: [
+            { title: 'Meeting', start: '2024-12-15' },
+            { title: 'Conference', start: '2024-12-20', end: '2024-12-22' }
+        ]
+    });
+
+    calendar.render();
+}
