da01de9dce03d6c8897e3dfb601fd9f8c11add44
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2024-12-12 16:27:04 +0200

    seeding

--- BeHealthy/BeHealthy/wwwroot/js/calendar.js
+++ BeHealthy/BeHealthy/wwwroot/js/calendar.js
@@ -3,10 +3,45 @@
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
+        headerToolbar: {
+            start: 'prev,next today', // navigation buttons
+            center: 'title',         // calendar title
+            end: 'dayGridMonth,timeGridWeek,timeGridDay' // view options
+        },
         events: [
-            { title: 'Meeting', start: '2024-12-15' },
-            { title: 'Conference', start: '2024-12-20', end: '2024-12-22' }
-        ]
+            {
+                title: 'Project Deadline',
+                start: '2024-12-20',
+                description: 'Final submission of the project',
+                backgroundColor: '#ff5733',
+                borderColor: '#c70039'
+            },
+            {
+                title: 'Team Meeting',
+                start: '2024-12-15T10:00:00',
+                end: '2024-12-15T12:00:00',
+                description: 'Discuss project progress',
+                backgroundColor: '#33ff57',
+                borderColor: '#33ff57'
+            },
+            {
+                title: 'Holiday',
+                start: '2024-12-25',
+                allDay: true, // marks this as an all-day event
+                description: 'Christmas holiday'
+            },
+            {
+                title: 'Conference',
+                start: '2024-12-28T09:00:00',
+                end: '2024-12-28T17:00:00',
+                description: 'Annual tech conference',
+                backgroundColor: '#3375ff',
+                borderColor: '#3375ff'
+            }
+        ],
+        eventClick: function (info) {
+            alert('Event: ' + info.event.title + '\nDescription: ' + info.event.extendedProps.description);
+        }
     });
 
     calendar.render();
