b48336b0788ac8c985d76912cfbdc2f8c8959ba3
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2025-02-21 15:25:10 +0200

    changed connection string

--- BeHealthy.Infrastructure/Data/ApplicationDbContextFactory.cs
+++ BeHealthy.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -8,7 +8,8 @@ namespace BeHealthy.Infrastructure.Data
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
-            var connectionString = "Host=behealthydb;Port=5432;Database=behealthy;Username=admin;Password=7530";
+            //var connectionString = "Host=behealthydb;Port=5432;Database=behealthy;Username=admin;Password=7530";
+            var connectionString = "Host=dpg-cus7h6rv2p9s73ataba0-a;Port=5432;Database=behealthy_w1ur;Username=behealthy_w1ur_user;Password=MAAzUNXPSpDiGW7weNC3voseF8FL8zq2";
 
             if (string.IsNullOrEmpty(connectionString))
             {
