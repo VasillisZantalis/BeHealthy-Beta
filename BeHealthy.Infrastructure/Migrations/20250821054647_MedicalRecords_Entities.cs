8aa0cf8b885d4b45a8cb4e34b88ff3bc0af18704
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2025-08-21 08:48:43 +0300

    #34 generated migrations

---
+++ BeHealthy.Infrastructure/Migrations/20250821054647_MedicalRecords_Entities.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BeHealthy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MedicalRecords_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TreatmentId",
+                table: "Prescriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TreatmentId1",
+                table: "Prescriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Allergies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AllergyName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Allergen = table.Column<string>(type: "text", nullable: true),
+                    Severity = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    PatientId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Allergies_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MedicalRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Notes = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    CreatedUserId = table.Column<string>(type: "text", nullable: true),
+                    RecordDate = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    PatientId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MedicalRecords_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VisitDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Reason = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Notes = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    PatientId = table.Column<int>(type: "integer", nullable: false),
+                    DoctorId = table.Column<int>(type: "integer", nullable: false),
+                    MedicalRecordId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Visits_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Visits_MedicalRecords_MedicalRecordId",
+                        column: x => x.MedicalRecordId,
+                        principalTable: "MedicalRecords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Visits_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diagnoses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Notes = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    VisitId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diagnoses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Diagnoses_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LabResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TestName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    ResultValue = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Unit = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    ReferenceRange = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ResultDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    VisitId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LabResults_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    VisitId = table.Column<int>(type: "integer", nullable: false),
+                    DiagnosisId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Treatments_Diagnoses_DiagnosisId",
+                        column: x => x.DiagnosisId,
+                        principalTable: "Diagnoses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Treatments_Visits_VisitId",
+                        column: x => x.VisitId,
+                        principalTable: "Visits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_TreatmentId",
+                table: "Prescriptions",
+                column: "TreatmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_TreatmentId1",
+                table: "Prescriptions",
+                column: "TreatmentId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allergies_PatientId",
+                table: "Allergies",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diagnoses_VisitId",
+                table: "Diagnoses",
+                column: "VisitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabResults_VisitId",
+                table: "LabResults",
+                column: "VisitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalRecords_PatientId",
+                table: "MedicalRecords",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatments_DiagnosisId",
+                table: "Treatments",
+                column: "DiagnosisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatments_VisitId",
+                table: "Treatments",
+                column: "VisitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_DoctorId",
+                table: "Visits",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_MedicalRecordId",
+                table: "Visits",
+                column: "MedicalRecordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visits_PatientId",
+                table: "Visits",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId",
+                table: "Prescriptions",
+                column: "TreatmentId",
+                principalTable: "Treatments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId1",
+                table: "Prescriptions",
+                column: "TreatmentId1",
+                principalTable: "Treatments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId",
+                table: "Prescriptions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId1",
+                table: "Prescriptions");
+
+            migrationBuilder.DropTable(
+                name: "Allergies");
+
+            migrationBuilder.DropTable(
+                name: "LabResults");
+
+            migrationBuilder.DropTable(
+                name: "Treatments");
+
+            migrationBuilder.DropTable(
+                name: "Diagnoses");
+
+            migrationBuilder.DropTable(
+                name: "Visits");
+
+            migrationBuilder.DropTable(
+                name: "MedicalRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Prescriptions_TreatmentId",
+                table: "Prescriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Prescriptions_TreatmentId1",
+                table: "Prescriptions");
+
+            migrationBuilder.DropColumn(
+                name: "TreatmentId",
+                table: "Prescriptions");
+
+            migrationBuilder.DropColumn(
+                name: "TreatmentId1",
+                table: "Prescriptions");
+        }
+    }
+}
