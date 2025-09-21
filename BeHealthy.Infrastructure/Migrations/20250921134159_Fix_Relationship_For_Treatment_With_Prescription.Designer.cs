bda7e10fb0af3b6c35355bcb8ea12c53ae609bdd
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2025-09-21 17:10:07 +0300

    re-initialized docker-compose

--- BeHealthy.Infrastructure/Migrations/20250921134159_Fix_Relationship_For_Treatment_With_Prescription.Designer.cs
+++ BeHealthy.Infrastructure/Migrations/20250921134159_Fix_Relationship_For_Treatment_With_Prescription.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BeHealthy.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BeHealthy.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921134159_Fix_Relationship_For_Treatment_With_Prescription")]
+    partial class Fix_Relationship_For_Treatment_With_Prescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BeHealthy.Infrastructure/Migrations/20250921134159_Fix_Relationship_For_Treatment_With_Prescription.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeHealthy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Relationship_For_Treatment_With_Prescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId1",
+                table: "Prescriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Prescriptions_TreatmentId1",
+                table: "Prescriptions");
+
+            migrationBuilder.DropColumn(
+                name: "TreatmentId1",
+                table: "Prescriptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TreatmentId1",
+                table: "Prescriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_TreatmentId1",
+                table: "Prescriptions",
+                column: "TreatmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prescriptions_Treatments_TreatmentId1",
+                table: "Prescriptions",
+                column: "TreatmentId1",
+                principalTable: "Treatments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
